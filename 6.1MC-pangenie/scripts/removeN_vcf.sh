9e84aa04ef7ad3b7a5dbc9067bfb2e4284d2db41
WengangXbio <85557809+WengangXbio@users.noreply.github.com>
2024-07-08 17:36:08 +0800

    Create removeN_vcf.sh

---
+++ 6.1MC-pangenie/scripts/removeN_vcf.sh
@@ -0,0 +1,41 @@
+grep -v "^#" cattle31.raw.vcfbub.vcf.diploid > cattle31.raw.vcfbub.vcf.diploid.noheader
+split -n l/150 cattle31.raw.vcfbub.vcf.diploid.noheader cattle31.raw.vcfbub.vcf.diploid.noheader
+ls cattle31.raw.vcfbub.vcf.diploid.noheader* | parallel -j 150 ./removeN.sh {}
+
+#cat removeN.sh 
+input=$1
+mkdir ${input}_temp
+echo "" > ${input}.NNN.txt
+cat ${input} | while IFS= read -r line; do
+Ngt=$(echo $line |awk '{print $5}' |tr ',' '\n' |grep -n "N" |awk -F':' '{print $1}')
+if [ -n "$Ngt" ]; then
+gt=$(echo $line |cut -d ' ' -f10-)
+echo $line |awk '{print $5}' |tr ',' '\n' |awk '{print NR,NR,$0}' > ${input}_temp/alt_seq
+for agt in `echo $Ngt` ; do
+gt=$(echo $gt |tr ' ' '\n' |awk -F'|' -v a=$agt 'BEGIN{OFS=""}{if ($1==a) $1="."; if ($2==a) $2="."; print $1,"|",$2}')
+awk -v a=$agt '$1!=a' ${input}_temp/alt_seq > ${input}_temp/temp
+mv ${input}_temp/temp ${input}_temp/alt_seq
+done
+echo $line |awk 'BEGIN{OFS="\t"} {print $1,$2,$3,$4}' > ${input}_temp/c1234_txt
+awk '{print $3}' ${input}_temp/alt_seq |tr '\n' ',' |sed 's/,$//' |awk '{print $0'\n'}' > ${input}_temp/c5_txt
+echo $line |awk 'BEGIN{OFS="\t"} {print $6,$7,$8,$9}' > ${input}_temp/c6789_txt
+echo $gt |tr ' ' '\t' > ${input}_temp/gt_txt
+awk '$2=NR {print $1,$2}' ${input}_temp/alt_seq |while IFS= read -r yty; do
+old=$(echo $yty |awk '{print $1}')
+new=$(echo $yty |awk '{print $2}')
+cat ${input}_temp/gt_txt | tr '\t' '\n' | awk -F'|' -v a="$old" -v b="$new" 'BEGIN{OFS=""}{if ($1==a) $1=b; if ($2==a) $2=b; print $1,"|",$2}' > ${input}_temp/temp_gt
+cat ${input}_temp/temp_gt | tr '\n' '\t' > ${input}_temp/gt_txt
+rm ${input}_temp/temp_gt
+done
+lnl=$(cat ${input}_temp/c5_txt |wc -l )
+if [ "$lnl" -gt 0 ]; then
+paste -d '\t' ${input}_temp/c1234_txt ${input}_temp/c5_txt ${input}_temp/c6789_txt ${input}_temp/gt_txt
+echo  $line |awk '{print $1,$2}' >> ${input}.NNN.txt
+else
+echo  $line |awk '{print $1,$2,"revomal"}' >> ${input}.NNN.txt
+fi
+rm ${input}_temp/c1234_txt ${input}_temp/c5_txt ${input}_temp/c6789_txt ${input}_temp/gt_txt ${input}_temp/alt_seq
+else
+echo $line |sed 's/ /\t/g'
+fi
+done > ${input}.removeN
